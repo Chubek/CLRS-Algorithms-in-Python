b5ac2caaba60712eb34bcbcdbc3daf4875d3cda8
Chubak Bidpaa <aurlito@riseup.net>
2019-04-07 19:00:46 +0430

    bubblesort_fin

--- 003-bubble-sort.py
+++ 003-bubble-sort.py
@@ -1,14 +1,22 @@
 #bubblesort
 
-def swap(a, b):
-    temp = a
-    a = b
-    b = temp
+def swap(A, a, b):
+    temp = A[a]
+    A[a] = A[b]
+    A[b] = temp
+
+    return A
 
 def bubblesort(A):
     for i in range(len(A) - 1):
-        for j in range(len(A), i + 1):
+        for j in range(len(A) - 1, i, -1):
             if A[j] < A[j - 1]:
-                swap(A[j], A[j - 1])
+                A = swap(A, j, j - 1)
+
+    return A
 
 
+
+A = [5, 2, 1, 6, 3, 6, 4]
+
+print(bubblesort(A))
